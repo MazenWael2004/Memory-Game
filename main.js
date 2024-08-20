549ca3c5fb1126517b739cc3b7e0de86f9afd332
MazenWael2004 <115092034+MazenWael2004@users.noreply.github.com>
2024-08-20 18:49:56 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+
+var selected = [];
+var boxes = document.querySelectorAll(".item");
+for (var i = 0; i < boxes.length; i++) {
+    var box = boxes[i];
+    box.addEventListener("click", function() {
+        if(!this.classList.contains("flipped")){
+        selected.push(this);
+        this.classList.toggle("flipped");
+        }
+
+        if(selected.length % 2 == 0){
+            CheckIfEqual(selected[selected.length-1].childNodes[3],selected[selected.length-2].childNodes[3]);
+        }
+
+        IfGameWon(selected);
+    });
+}
+
+function CheckIfEqual(img1,img2){ // To check if two images chosen are matched or not.
+    if(img1.src == img2.src){
+        var audio = new Audio("sounds/correct.mp3");
+        audio.play();
+    }
+    else{
+        var audio = new Audio("sounds/lose.mp3");
+        audio.play();
+
+        setTimeout(function(){
+            selected[selected.length-1].classList.toggle("flipped");
+            selected[selected.length-2].classList.toggle("flipped");
+            selected.pop();
+            selected.pop();
+            
+        },500);
+       
+    }
+}
+
+function IfGameWon(array){
+ if(array.length == 20){
+    alert("YOU WONNNNN!");
+    var audio = new Audio("sounds/cheering.mp3");
+    audio.play();
+ }
+}
